8584fb35e7829ddfa8c1535d81a7fb12549ad011
Adam <Adamvys@gmail.com>
2024-11-08 14:13:58 +0100

    html and css templates

---
+++ ITStep/ITStep/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+    # return HttpResponse("Hello World. I am home")
+    return render(request, 'home.html')
+def about(request):
+    # return HttpResponse("My About page.")
+    return render(request, 'about.html')
